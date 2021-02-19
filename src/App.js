1c043165c4d51d96ca806272d651dc9cc3365f16
Kevin Nguyen Vo <nguyen.vo@oculus.com>
2021-02-19 05:54:36 -0800

    First Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./assets/scss/app.scss"
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+
+function Sidebar() {
+    return (
+        <div className="sidebar" >
+            <div className="sidebar-wrapper">
+                <p>My React Components</p>
+                <hr />
+                <Nav vertical>
+                    <NavItem>
+                        <NavLink href="#">Buttons</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Grid System</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Panels</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Sweet Alert</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Notifications</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Icons</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Typography</NavLink>
+                    </NavItem>
+                </Nav>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
